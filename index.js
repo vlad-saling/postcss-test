da223acd7c62322cf0e8188fdd087f1f10d1933b
vlad-saling <vladislavsaling@gmail.com>
2017-01-11 18:34:59 +0100

    adding variables option

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const fs = require('fs');
 const postcss  = require('postcss');
 const autoprefixer = require('autoprefixer');
+const postcssSimpleVars = require('postcss-simple-vars');
 
 fs.readFile('style.source.css', (err, css) => {
-    postcss([autoprefixer])
+    postcss([autoprefixer, postcssSimpleVars])
         .process(css, { from: 'tyle.source.css', to: 'style.css' })
         .then(result => {
             fs.writeFile('style.css', result.css);
