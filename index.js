e6a2cbc4f3d9d47986686b7ad659e70f15690423
vlad-saling <vladislavsaling@gmail.com>
2017-01-11 18:46:31 +0100

    adding more modules

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ const fs = require('fs');
 const postcss  = require('postcss');
 const autoprefixer = require('autoprefixer');
 const postcssSimpleVars = require('postcss-simple-vars');
+const postcssNested = require('postcss-nested');
+const postcssMixins = require('postcss-mixins');
+const postcssMinify = require('postcss-minify-selectors');
 
 fs.readFile('style.source.css', (err, css) => {
-    postcss([autoprefixer, postcssSimpleVars])
+    postcss([autoprefixer, postcssSimpleVars, postcssNested, postcssMixins, postcssMinify])
         .process(css, { from: 'tyle.source.css', to: 'style.css' })
         .then(result => {
             fs.writeFile('style.css', result.css);
