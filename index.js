e43e1e931e9dec27ae0df5befb1dfb79b76573b4
vlad-saling <vladislavsaling@gmail.com>
2017-01-12 11:10:26 +0100

    swapping minifiers

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const autoprefixer = require('autoprefixer');
 const postcssSimpleVars = require('postcss-simple-vars');
 const postcssNested = require('postcss-nested');
 const postcssMixins = require('postcss-mixins');
-const postcssMinify = require('postcss-minify-selectors');
+const cssnano = require('cssnano');
 
 fs.readFile('style.source.css', (err, css) => {
-    postcss([autoprefixer, postcssSimpleVars, postcssNested, postcssMixins, postcssMinify])
+    postcss([autoprefixer, postcssSimpleVars, postcssNested, postcssMixins, cssnano])
         .process(css, { from: 'tyle.source.css', to: 'style.css' })
         .then(result => {
             fs.writeFile('style.css', result.css);
